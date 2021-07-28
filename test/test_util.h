dd37f58204af1228858861290e11493fa691254a
sequpt <sequpt@protonmail.com>
2021-07-28 22:29:42 +0000

    Add `test_util.h`

---
+++ test/test_util.h
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: 0BSD
+/*!
+ * @file
+ * @licence{
+ * BSD Zero Clause License
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ * }
+ */
+/*==============================================================================
+    GUARD
+==============================================================================*/
+#ifndef TEST_UTIL_H_20210728024822
+#define TEST_UTIL_H_20210728024822
+/*==============================================================================
+    PREPROCESSOR
+==============================================================================*/
+#define STRINGIFY(...)     STRINGIFY_RAW(__VA_ARGS__)
+#define STRINGIFY_RAW(...) #__VA_ARGS__
+/*==============================================================================
+    GUARD
+==============================================================================*/
+#endif // TEST_UTIL_H_20210728024822
